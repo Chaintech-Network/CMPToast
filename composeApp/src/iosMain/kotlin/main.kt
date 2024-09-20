555cd6cd85f149f4c142bf637b7bddd1beadec21
Rehan <143475887+ctn-mobile-innovation@users.noreply.github.com>
2024-09-20 08:25:00 +0530

    Delete composeApp directory

--- composeApp/src/iosMain/kotlin/main.kt
+++
@@ -1,5 +0,0 @@
-import androidx.compose.ui.window.ComposeUIViewController
-import network.chaintech.cmptoastdemo.App
-import platform.UIKit.UIViewController
-
-fun MainViewController(): UIViewController = ComposeUIViewController { App() }
