b069d29afd0271cc8848ef82602981da1ecf6761
ctn-mobile-innovation <rehan.t@chaintech.network>
2025-03-26 07:58:38 +0000

    v1.0.5 - Default Padding Added

--- composeApp/src/commonMain/kotlin/network/chaintech/cmptoastdemo/App.kt
+++ composeApp/src/commonMain/kotlin/network/chaintech/cmptoastdemo/App.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
